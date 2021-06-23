a62d118c97895686b82fdf1ebb1d3945c5d09001
Daniel Pacheco <daniel_amorimpacheco@hotmail.com>
2021-06-23 20:02:54 -0300

    APS

---
+++ firebaseConfig.js
@@ -0,0 +1,15 @@
+import firebase from 'firebase';
+
+var firebaseConfig = {
+  apiKey: "AIzaSyCYky6cldOgRrPtPmnZeVtYk1GnOQ0ug5Y",
+  authDomain: "aps-firebase-fac74.firebaseapp.com",
+  projectId: "aps-firebase-fac74",
+  storageBucket: "aps-firebase-fac74.appspot.com",
+  messagingSenderId: "286014051575",
+  appId: "1:286014051575:web:b76f8a4a02a01b1542aff2",
+  measurementId: "G-05Z7SEKDGP"
+  };
+  // Initialize Firebase
+  firebase.initializeApp(firebaseConfig);
+
+ export default firebase; 
