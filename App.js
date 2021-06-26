45da38624621d79e7e5410ce40248c05924ce51b
Daniel Pacheco <daniel_amorimpacheco@hotmail.com>
2021-06-25 22:38:37 -0300

    login/logout

--- App.js
+++ App.js
@@ -1,98 +1,31 @@
 import React, { useState, useEffect } from 'react';
 import { StyleSheet, Text, View, TextInput, TouchableOpacity } from 'react-native';
 import firebase from './firebaseConfig';
+import {NavigationContainer} from '@react-navigation/native' 
+import {createStackNavigator} from '@react-navigation/stack' 
+
+import Home from './src/pages/Home';
+
+import Cadastro from './src/pages/Cadastro';
+
+const Stack = createStackNavigator();
+static navigation
 
 export default function App() {
-  const [email, setEmail] = useState('');
-  const [senha, setSenha] = useState('');
-
-  function loginFirebase(){
-    firebase.auth().signInWithEmailAndPassword(email, senha).catch(function(error) {
-      var errorCode = error.code;
-      var errorMessage = error.message;
-      alert(errorCode, errorMessage);
-      // ...
-    });
-  }
-
-  useEffect(()=>{
-    firebase.auth().onAuthStateChanged(function(user) {
-      if (user) {
-       console.log("Logado"+user.uid)
-      } else {
-        console.log('não logado!')
-      }
-    });
-  },[])
-
-  function logOutFirebase(){
-    firebase.auth().signOut().then(function() {
-      alert('Deslogado com sucesso"')
-    }).catch(function(error) {
-      alert('Falha"')
-    });
-  }
-
-  function createUserFirebase(){
-    firebase.auth().createUserWithEmailAndPassword(email, senha).catch(function(error) {
-      var errorCode = error.code;
-      var errorMessage = error.message;
-      alert(errorCode, errorMessage);
-    });
-  }
-
-
 
   return (
-    <View style={styles.container}>
-      <Text style={styles.text}>LOGIN</Text>
+    <NavigationContainer>
+      <Stack.Navigator initialRouteName = "Home">
+        <Stack.Screen 
+          name = "Home"
+          component = {Home}
+          options ={{
+            headerLeft: null
+          }}/>
+        <Stack.Screen name = "Cadastro" component = {Cadastro} />
+      </Stack.Navigator>
 
-      <TextInput style={styles.input} placeholder='digite seu e-mail' onChangeText={email => setEmail(email)} value={email} />
-
-      <TextInput style={styles.input} placeholder='digite sua senha' onChangeText={senha => setSenha(senha)} value={senha} />
-  
-      <TouchableOpacity style={styles.button} onPress={()=>{ loginFirebase()}}>
-        <Text>Login</Text>
-      </TouchableOpacity>
-
-      <TouchableOpacity style={styles.button} onPress={()=>{ logOutFirebase()}}>
-        <Text>LogOut</Text>
-      </TouchableOpacity>
-
-      <TouchableOpacity style={styles.button} onPress={()=>{ createUserFirebase()}}>
-        <Text>Criar Usuario</Text>
-      </TouchableOpacity>
-
-    </View>
+    </NavigationContainer>
+   
   );
 }
-
-const styles = StyleSheet.create({
-  container: {
-    flex: 1,
-    backgroundColor: '#fff',
-    alignItems: 'center',
-    justifyContent: 'center',
-  },
-  text: {
-    color: 'red',
-    fontSize: 15
-  },
-  input: {
-    width: 250,
-    height: 60,
-    borderWidth: 1,
-    borderColor: 'red',
-    marginTop: 10,
-    textAlign: 'center'
-  },
-  button: {
-    marginTop: 15,
-    width: 250,
-    height: 60,
-    borderWidth: 1,
-    borderColor: 'red',
-    justifyContent: 'center',
-    alignItems: 'center'
-  }
-});
